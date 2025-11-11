04a09c076c775b3cc81f50438dbca34200735ecd
Massimo Nocentini <massimo.nocentini@gmail.com>
2025-11-11 11:24:05 +0100

    Minor fix on the makefile

--- src/test/json-bench.scm
+++ src/test/json-bench.scm
@@ -19,8 +19,8 @@
 (display "Simdjson egg, users_100k.json benchmark:\n")
 (time (simdjson-load/ondemand "./test/users_100k.json"))
 
-(display "Json abnf egg, users_1.7m.json benchmark:\n")
-(time (call-with-input-file "./test/users_1.7m.json" (λ (port) (parser (read-string #f port)))))
+;(display "Json abnf egg, users_1.7m.json benchmark:\n")
+;(time (call-with-input-file "./test/users_1.7m.json" (λ (port) (parser (read-string #f port)))))
 
 (display "Simdjson egg, users_1.7m.json benchmark:\n")
 (time (simdjson-load/ondemand "./test/users_1.7m.json"))
