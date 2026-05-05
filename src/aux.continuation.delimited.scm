1fe870caa2cf99987eaa761495900e0552d22d71
Massimo Nocentini <massimo.nocentini@gmail.com>
2026-05-05 18:35:04 +0200

    Refactor resetcc and letcc/shift syntax rules to remove callcc usage for improved clarity and consistency

--- src/aux.continuation.delimited.scm
+++ src/aux.continuation.delimited.scm
@@ -6,28 +6,28 @@
           (aux base)
           (aux continuation))
 
-  (define-syntax-rule (resetcc body ...) (callcc (delimcc-reset (τ body ...))))
+  (define-syntax-rule (resetcc body ...) (delimcc-reset (τ body ...)))
   (define-syntax-rule (resetcc+null body ...) (resetcc body ... '()))
   (define-syntax define-resetcc
     (syntax-rules ()
       ((define-resetcc (def arg ...) body ...) (define def (λ (arg ...) (resetcc body ...))))
       ((define-resetcc def body ...) (define def (resetcc body ...)))))
   
-  (define-syntax-rule (letcc/shift k body ...) (callcc (delimcc-shift (λ (k) body ...))))
+  (define-syntax-rule (letcc/shift k body ...) (delimcc-shift (λ (k) body ...)))
   (define (callcc/shift f) (letcc/shift k (f k)))
   (define-syntax-rule (λ-shift args body ...) (letcc/shift k (λ args (let1 (x (begin body ...)) (k x)))))
   (define-syntax-rule (τ-shift body ...) (λ-shift () body ...))
 
-  (define *delimcc-stack* identity) ; or: (λ (v) (error "Missing enclosing resetcc" v)) for a more strict experience.
-  (define (delimcc-stack-abort v) (*delimcc-stack* v))
-  (define (delimcc-stack-push! k) (set! *delimcc-stack* k))
-  (define (delimcc-stack-pop!) *delimcc-stack*)
-  (define ((delimcc-reset t) k)
-    (let* ((s (delimcc-stack-pop!))
-           (s* (λ (r) (delimcc-stack-push! s) (k r))))
-      (delimcc-stack-push! s*)
-      (delimcc-stack-abort (t))))
-  (define ((delimcc-shift h) k) (delimcc-stack-abort (h (λ (v) (resetcc (k v))))))
+  (define *meta-continuation* identity) ; or: (λ (v) (error "Missing enclosing resetcc" v)) for a more strict experience.
+  (define (delimcc-abort thunk) (*meta-continuation* (thunk)))
+  
+  (define (delimcc-reset thunk)
+    (let1 (mc *meta-continuation*)
+      (letcc k
+        (set! *meta-continuation* (λ (v) (set! *meta-continuation* mc) (k v)))
+        (delimcc-abort thunk))))
+
+  (define (delimcc-shift f) (letcc k (delimcc-abort (τ (f (λ (v) (resetcc (k v))))))))
 
   (define (delimcc-extract) (letcc/shift k k))
   (define (delimcc-discard v) (letcc/shift _ v))
